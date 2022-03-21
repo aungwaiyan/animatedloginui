245c0d934e3658d57211e384b3edc889ca6518bd
Aung Wai Yan Htun <htunwaiyan817@gmail.com>
2022-03-21 20:33:21 +0630

    jsproject

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// UI 
+
+const loginsection = document.querySelector(".login"),
+      registersection = document.querySelector('.register'),
+      forgotsection = document.querySelector('.forgot');
+
+const loginlink = document.querySelector('.login-link'),
+      registerlink = document.querySelector('.register-link'),
+      forgotlink = document.querySelector('.forgot-link');
+
+const closebtn = document.querySelector('.far');
+
+registerlink.addEventListener('click',()=>{
+    loginsection.style.display="none";
+    registersection.style.display="flex";
+});
+
+loginlink.addEventListener('click',()=>{
+    registersection.style.display="none";
+    loginsection.style.display="flex";
+});
+
+forgotlink.addEventListener('click',()=>{
+    loginsection.style.display="none";
+    forgotsection.style.display="flex";
+});
+
+closebtn.addEventListener('click',()=>{
+    forgotsection.style.display="none";
+    loginsection.style.display="flex";
+})
+
+const icon = document.querySelector('.icon')
+const btn = document.querySelector('#btn-primary');
+
+btn.addEventListener('click',()=>{
+    // console.log("hay");
+
+    icon.classList.toggle('show');
+})
